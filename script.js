fd857e16add092ceef9771ac12e30d4c76121715
Felix <felixomosh7@gmail.com>
2023-12-25 11:07:49 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+const h2 = document.createElement("h2");
+h2.textContent = "This content has been added by JavaScript";
+
+document.querySelector("body").appendChild(h2);
+*/
+
+const button = document.getElementById('button');
+button.addEventListener("click", function(){
+    alert('You are amazing!');
+})
+
+function clickMe() {
+    alert('Welcome to Felix\'s world!');
+}
